0b3c5a2ff0b604f25a702296d30fa88f54669074
Mohammad Ahmed <yahmed786222@gmail.com>
2020-09-21 07:40:28 +0000

    Used hooks to add state inside a function component

--- src/routes/Registry.js
+++ src/routes/Registry.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function Registry() {
+    const [registryData, setRegistryData] = useState([]);
+    const [textInput, setTextInput] = useState("");
+    
+    const addItem = (e) => {
+        e.preventDefault();
+        const tempDate = [...registryData];
+        tempDate.push(textInput);
+        setRegistryData(tempDate);
+        setTextInput("");
+    }
+    console.log(registryData);
+
     return (
         <div>
             <h1>Registry</h1>
             <Link to="/">CLick here to go to Home</Link>
+            <form onSubmit={addItem}>
+                <label>Input: 
+                <input type="text" value={textInput}
+                    onChange={(e)=> setTextInput(e.target.value)}/>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+            
         </div>
     )
 }
